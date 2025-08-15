2fcc5bfbedcd03139b95331d8cba98a0013a013f
SiranjithIT <siranjithnewit@gmail.com>
2025-08-15 09:38:41 +0530

    Fetch Practice Modified

--- Template/async-fetch-prc/src/main.ts
+++ Template/async-fetch-prc/src/main.ts
@@ -18,14 +18,30 @@ const fetchItems = async() =>{
   todoList.forEach((item)=>{
     const card = document.createElement('div');
     card.className = "todo-card"
+    
+    let priorityClassName = "";
+    if(item.priority === 'High'){
+      priorityClassName = "high-priority"
+    }else{
+      priorityClassName = "other-priority"
+    }
+
+    let statusClassName = "";
+    if(item.status === "Done"){
+      statusClassName = "status-completed";
+    }else if(item.status === "Ongoing"){
+      statusClassName = "status-ongoing";
+    }else{
+      statusClassName = "status-new";
+    }
 
     card.innerHTML = `
       <h2>${item.task}</h2>
       <p>Category: ${item.category}</p>
       <p>Due Date: ${item.dueDate}</p>
       <div class="card-end">
-        <p>${item.priority} Priority</p>
-        <p>${item.status} </p>
+        <p class="${priorityClassName}">${item.priority} Priority</p>
+        <p class="${statusClassName}">${item.status} </p>
       </div>
     `
     container?.appendChild(card);
